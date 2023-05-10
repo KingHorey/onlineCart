3d6435d2e09945c4d80e24430c648366059c25ae
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-05-10 12:16:18 +0100

    cart design

---
+++ script.js
@@ -0,0 +1,8 @@
+const body = document.body
+
+const anchor = document.getElementById("cancel");
+anchor.style.textDecoration = "None";
+anchor.style.marginBottom = "1.5rem";
+
+const img = document.querySelector(".pricing .pricing-child div img");
+img.style.width = "2.5rem";
